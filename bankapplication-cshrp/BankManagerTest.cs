b1d2bf93d353e273c3a115cc0c2b6608a38e7be6
isaabake <isaac.baker@email.wsu.edu>
2013-11-13 17:12:26 -0800

    createAccount unit test written

--- bankapplication-cshrp/BankManagerTest.cs
+++ bankapplication-cshrp/BankManagerTest.cs
@@ -7,12 +7,26 @@ using Xunit;
 
 namespace bankapp
 {
-	class BankManagerTest
+	public class BankManagerTest
     {
         [Fact]
         public void createAccount()
         {
+            BankManager bm = new BankManager();
 
+            //bm.createAccount();
+
+            bm.Accounts[0].AccountNumber = 1;
+            bm.Accounts[0].Name = "TestAcct";
+            bm.Accounts[0].Balance = 50;
+            bm.Accounts[0].DateCreated = "1/1/11";
+            bm.AccountsAvailable[0] = false;
+
+            Assert.True(bm.Accounts[0].AccountNumber == 1);
+            Assert.True(bm.Accounts[0].Name == "TestAcct");
+            Assert.True(bm.Accounts[0].Balance == 50);
+            Assert.True(bm.Accounts[0].DateCreated == "1/1/11");
+            Assert.True(bm.AccountsAvailable[0] == false);
         }
     }
 }
