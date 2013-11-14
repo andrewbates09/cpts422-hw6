49242a2bcbbf7e84cf32145d409c0b0f586abc18
isaabake <isaac.baker@email.wsu.edu>
2013-11-13 17:41:11 -0800

    Added copyTo unit test

--- bankapplication-cshrp/AccountTest.cs
+++ bankapplication-cshrp/AccountTest.cs
@@ -19,5 +19,26 @@ namespace bankapp
             Assert.True(account.Name == "TestAcct");
             Assert.True(account.DateCreated == "1/1/11");
         }
+
+        [Fact]
+        public void copyAccount()
+        {
+            Account account = new Account(50.0, 1, "TestAcct", "1/1/11");
+            Account account2 = new Account(account);
+
+            Assert.True(account.Balance == account2.Balance);
+            Assert.True(account.AccountNumber == account2.AccountNumber);
+            Assert.True(account.Name == account2.Name);
+            Assert.True(account.DateCreated == account2.DateCreated);
+        }
+
+        [Fact]
+        public void copyAccount()
+        {
+            Account account = new Account(50.0, 1, "TestAcct", "1/1/11");
+            Account account2 = new Account(account);
+        }
+
+
     }
 }
